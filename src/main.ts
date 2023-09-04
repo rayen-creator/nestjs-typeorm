425de7c0134d943c62117d55eb5318842b6582d0
rayen-creator <rayenoueslati60@gmail.com>
2023-09-04 18:15:00 +0100

    ✨ added swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  //swagger implimentation
+  const config = new DocumentBuilder()
+    .setTitle('Online Task Management System')
+    .setDescription('Online Task Management System API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  //dto implimentation
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/task/dto/create-task.dto.ts
+++ src/modules/task/dto/create-task.dto.ts
@@ -1,1 +1,22 @@
-export class CreateTaskDto {}
+import { ApiProperty } from "@nestjs/swagger"
+import { IsDate, IsNotEmpty, IsString } from "class-validator"
+
+export class CreateTaskDto {
+
+    @ApiProperty({type:'string'})
+    @IsNotEmpty()
+    @IsString()
+    title: string
+    @ApiProperty({type:'string'})
+    @IsNotEmpty()
+    @IsString()
+    description: string
+    @ApiProperty({type:'string'})
+    @IsNotEmpty()
+    @IsDate()
+    dueDate: Date
+    @ApiProperty({type:'string'})
+    @IsNotEmpty()
+    @IsString()
+    priority: string
+}
