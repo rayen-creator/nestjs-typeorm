1d25b0707cedab4e7ad9cd794ead08afae991ed0
rayen-creator <rayenoueslati60@gmail.com>
2023-09-17 16:39:16 +0100

    ✅ update unit test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "nestjs api"', () => {
+      expect(appController.getHello()).toBe('nestjs api');
     });
   });
 });
